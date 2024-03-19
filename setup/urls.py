57443edffc8d3f3a391b8d0b4a974a9bd1a7fae3
lucialhev <luciahevicente@gmail.com>
2024-03-19 09:54:21 -0300

    criando model de Matricula

--- setup/urls.py
+++ setup/urls.py
@@ -16,12 +16,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
-from escola.views import AlunosViewSet, CursoViewSet
+from escola.views import AlunosViewSet, CursoViewSet, MatriculaViewSet
 from rest_framework import routers
 
 router = routers.DefaultRouter()
 router.register('alunos', AlunosViewSet, basename='Alunos')
 router.register('cursos', CursoViewSet, basename='Cursos')
+router.register('matriculas', MatriculaViewSet, basename='Matriculas')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
