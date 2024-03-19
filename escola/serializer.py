e5a4bff4f40917ff263f42dfa846806b34bb9bfa
lucialhev <luciahevicente@gmail.com>
2024-03-19 10:25:56 -0300

    Listagens personalizadas

--- escola/serializer.py
+++ escola/serializer.py
@@ -15,3 +15,18 @@ class Matriculaerializer(serializers.ModelSerializer):
     class Meta:
         model = Matricula
         fields = '__all__'
+
+class ListaMatriculasAlunoSerializer(serializers.ModelSerializer):
+    curso = serializers.ReadOnlyField(source='curso.descricao')
+    periodo = serializers.SerializerMethodField()
+    class Meta:
+        model = Matricula
+        fields =['curso','periodo']
+    def get_periodo(self,obj):
+        return obj.get_periodo_display()
+    
+class ListaAlunosMatriculadosSerializer(serializers.ModelSerializer):
+    aluno_nome = serializers.ReadOnlyField(source='aluno.nome')
+    class Meta:
+        model = Matricula
+        fields = ['aluno_nome']
--- escola/views.py
+++ escola/views.py
@@ -1,6 +1,6 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, generics
 from escola.models import Aluno,Curso,Matricula
-from escola.serializer import AlunoSerializer, CursoSerializer,Matriculaerializer
+from escola.serializer import AlunoSerializer, CursoSerializer,Matriculaerializer, ListaMatriculasAlunoSerializer, ListaAlunosMatriculadosSerializer
 
 class AlunosViewSet(viewsets.ModelViewSet):
     """ Exibindo todos os alunos"""
@@ -16,3 +16,17 @@ class MatriculaViewSet(viewsets.ModelViewSet):
     """ Exibindo todas as matriculas"""
     queryset = Matricula.objects.all()
     serializer_class = Matriculaerializer
+
+class ListaMatriculasAluno(generics.ListAPIView):
+    """ Listando as matriculas de um aluno"""
+    def get_queryset(self):
+        queryset = Matricula.objects.filter(aluno_id = self.kwargs['pk'])
+        return queryset
+    serializer_class = ListaMatriculasAlunoSerializer
+
+class ListaAlunosMatriculados(generics.ListAPIView):
+    """Listando alunos matriculados em um curso"""
+    def get_queryset(self):
+        queryset = Matricula.objects.filter(curso_id = self.kwargs['pk'])
+        return queryset
+    serializer_class = ListaAlunosMatriculadosSerializer
