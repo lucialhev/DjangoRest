ae4aa4099d266fa9da4a91c991fc09bc096fea9c
lucialhev <luciahevicente@gmail.com>
2024-03-19 09:34:03 -0300

    Criando os ViewSet e serializando

--- setup/urls.py
+++ setup/urls.py
@@ -15,10 +15,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from escola.views import alunos
+from django.urls import path,include
+from escola.views import AlunosViewSet, CursoViewSet
+from rest_framework import routers
+
+router = routers.DefaultRouter()
+router.register('alunos', AlunosViewSet, basename='Alunos')
+router.register('cursos', CursoViewSet, basename='Cursos')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('alunos/',alunos),
+    path('',include(router.urls)),
 ]
