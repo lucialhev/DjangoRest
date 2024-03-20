0a612681af4458717c41ccba2fe484b248cd8446
lucialhev <luciahevicente@gmail.com>
2024-03-20 09:53:11 -0300

    Nova versão do projeto

--- escola/admin.py
+++ escola/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from escola.models import Aluno, Curso, Matricula
 
 class Alunos(admin.ModelAdmin):
-    list_display = ('id', 'nome', 'rg', 'cpf', 'data_nascimento')
+    list_display = ('id','nome', 'rg', 'cpf', 'data_nascimento')
     list_display_links = ('id', 'nome')
     search_fields = ('nome',)
     list_per_page = 20
@@ -10,14 +10,14 @@ class Alunos(admin.ModelAdmin):
 admin.site.register(Aluno, Alunos)
 
 class Cursos(admin.ModelAdmin):
-    list_display = ('id','cod_curso', 'descricao')
-    list_display_links = ('id', 'cod_curso')
-    search_fields = ('cod_curso',)
+    list_display = ('id', 'codigo_curso', 'descricao')
+    list_display_links = ('id', 'codigo_curso')
+    search_fields = ('codigo_curso',)
 
 admin.site.register(Curso, Cursos)
 
 class Matriculas(admin.ModelAdmin):
-    list_display = ('id','aluno', 'curso','periodo')
+    list_display = ('id', 'aluno', 'curso', 'periodo')
     list_display_links = ('id', )
 
 admin.site.register(Matricula, Matriculas)
--- escola/serializer.py
+++ escola/serializer.py
@@ -1,30 +1,30 @@
 from rest_framework import serializers
-from escola.models import Aluno, Curso,Matricula
+from escola.models import Aluno, Curso, Matricula
 
 class AlunoSerializer(serializers.ModelSerializer):
     class Meta:
         model = Aluno
-        fields =['id', 'nome', 'rg','cpf','data_nascimento']
+        fields = ['id', 'nome', 'rg', 'cpf', 'data_nascimento']
 
 class CursoSerializer(serializers.ModelSerializer):
     class Meta:
         model = Curso
         fields = '__all__'
 
-class Matriculaerializer(serializers.ModelSerializer):
+class MatriculaSerializer(serializers.ModelSerializer):
     class Meta:
         model = Matricula
-        fields = '__all__'
+        exclude = []
 
 class ListaMatriculasAlunoSerializer(serializers.ModelSerializer):
     curso = serializers.ReadOnlyField(source='curso.descricao')
     periodo = serializers.SerializerMethodField()
     class Meta:
         model = Matricula
-        fields =['curso','periodo']
-    def get_periodo(self,obj):
+        fields = ['curso', 'periodo']
+    def get_periodo(self, obj):
         return obj.get_periodo_display()
-    
+
 class ListaAlunosMatriculadosSerializer(serializers.ModelSerializer):
     aluno_nome = serializers.ReadOnlyField(source='aluno.nome')
     class Meta:
